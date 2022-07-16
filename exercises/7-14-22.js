82656f3d60e0110234e5f9a6f4a039fb0a880601
pknelson01 <pk.elliott11@gmail.com>
2022-07-15 21:49:38 -0600

    new content

--- exercises/7-14-22.js
+++ exercises/7-14-22.js
@@ -2,7 +2,7 @@ function print(x) {
     console.log(x);
 };
 
-// attempt 1
+// Exercise 1 - attempt 1
 
 // var myFunction = function (arrayOne, slice) {
 //     for (let i = 0; i < arrayOne.length; i++) {
@@ -18,23 +18,23 @@ function print(x) {
 // console.log(myFunction(z, 9));
 // console.log(z);
 
-// attempt 2
+// Exercise 1 - attempt 2
 
-// var mySecondFunc = function (arrayTwo, snipe) {
-//     for (let i = 0; i < arrayTwo.length; i++) {
-//         if (arrayTwo[i] == snipe) {
-//             arrayTwo.splice(i, 1)
-//             i--;
-//         }
-//     }
-//     return arrayTwo.length;
-// }
+var mySecondFunc = function (arrayTwo, snipe) {
+    for (let i = 0; i < arrayTwo.length; i++) {
+        if (arrayTwo[i] == snipe) {
+            arrayTwo.splice(i, 1)
+            i--;
+        }
+    }
+    return arrayTwo.length;
+}
 
-// let p = [1, 2, 2, 2, 3, 4, 5, 6, 7, 8, 9]
-// print(`The Length is: ${mySecondFunc(p, 2)}`)
-// print(p)
+let p = [1, 2, 2, 2, 3, 4, 5, 6, 7, 8, 9]
+print(`The Length is: ${mySecondFunc(p, 2)}`)
+print(p)
 
-// attempt 3
+// Exercise 1 - attempt 3
 
 // var myThirdFunc = function (arrayThree, snipe) {
 //     for (let i = 1; i < arrayThree.length; i++) {
@@ -44,13 +44,34 @@ function print(x) {
 // let d = [1, 2, 2, 3, 4, 5, 6, 7, 9]
 // myThirdFunc(d)
 
-function weReal(numNew) {
-    for (let a = 1; a < numNew; a++) {
-        let b = "";
-        for (let c = 1; c <= a; c++) {
-            b += c + " ";
-        }
-        print(b)
-    }
-}
-print(weReal(8))
+// Exercise 2 - attempt 1
+
+// function weReal(numNew) {
+//     for (let a = 1; a < numNew; a++) {
+//         let b = "";
+//         for (let c = 1; c <= a; c++) {
+//             b += c + " ";
+//         }
+//         print(b)
+//     }
+// }
+// print(weReal(8))
+
+// Exercise 3 - attempt 1
+
+// var myFunc = function max(x) {
+//     let largestNum = 0;
+//     for (i = 0; i < x.length; i++) {
+//         largestNum < x[i] ? largestNum = x[i] : largestNum = largestNum;
+//         // ternary Operator ^
+//     } return largestNum
+//     //     if (x[i] > largestNum) {
+//     //         largestNum = x[i]
+//     //     }
+//     // }
+//     // return largestNum
+// }
+
+// let x = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// print(x)
+// print(`the largest number is: ${myFunc(x)}`)

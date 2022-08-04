065891aa743c01e07f14da17d01b576546c6845c
pknelson01 <pk.elliott11@gmail.com>
2022-08-03 18:59:15 -0600

    new

--- codewars.js
+++ codewars.js
@@ -178,3 +178,35 @@ function CreatePhoneNumber(numbers) {
     console.log(decodeMorse('... --- ... -.-.--'));
 
 */
+
+/* Remove string spaces
+
+    function noSpace(x) {
+        let string = x.split(' ').join('')
+        return string
+    }
+
+    console.log(noSpace('Hello There'))
+
+*/
+
+/* Lost without a map
+
+
+
+*/
+
+// function maps(x) {
+//     for (let i in x) {
+//         x[i] = x[i] * 2
+//     }
+//     return x;
+// }
+
+// console.log(maps([4, 5, 2, 7, 1, 8]))
+
+function maps(x) {
+    return x.map(i => i * 2);
+}
+
+maps([1, 2, 3]);

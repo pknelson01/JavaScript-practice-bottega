1c011df9f442c6ed226aff61dc441157d6f0bc9e
pknelson01 <pk.elliott11@gmail.com>
2022-08-08 18:56:12 -0600

    new

--- ModuleSection/src/bootstrap.js
+++ ModuleSection/src/bootstrap.js
@@ -1,1 +1,7 @@
-console.log('Hello There')
+// import { greeting, mult, add } from './helper';
+
+// console.log(greeting, mult(2, 5), add(5, 11, 22));
+
+import * as helper from './helper';
+
+console.log(helper.greeting, helper.mult(2, 5), helper.add(5, 11, 22));
---
+++ ModuleSection/src/helper.js
@@ -0,0 +1,9 @@
+export const greeting = 'Hello there';
+
+export function mult(x, y) {
+    return x * y;
+}
+
+export function add(x, y, z) {
+    return x + y + z
+}

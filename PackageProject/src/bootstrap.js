cab27b8ca28cabab674f1664dca57e30cd46cd60
pknelson01 <pk.elliott11@gmail.com>
2022-08-08 21:24:27 -0600

    new

--- PackageProject/src/bootstrap.js
+++ PackageProject/src/bootstrap.js
@@ -1,2 +1,21 @@
-// ES6 code goes here
-console.log('es6 code goes here');
+import moment from 'moment';
+
+console.log(moment.now());
+
+const rightNow = moment();
+
+console.log(rightNow);
+
+const birthday = moment('2001-09-10', 'YYYY-MM-DD');
+
+console.log(birthday.fromNow());
+
+console.log(birthday.format('MMMM Do YYYY'));
+
+const twoWeeksFromNow = moment().add(2, 'weeks');
+
+console.log(twoWeeksFromNow.toString());
+
+const sixMonthsAgo = moment().subtract(6, 'months');
+
+console.log(sixMonthsAgo.toString());

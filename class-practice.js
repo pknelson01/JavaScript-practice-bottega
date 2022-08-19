b3a71c5bbf15cbb5e0f394499658e4b444746158
pknelson01 <pk.elliott11@gmail.com>
2022-08-18 20:18:04 -0600

    new

--- class-practice.js
+++ class-practice.js
@@ -1,3 +1,9 @@
+// Create a class that represents a Vehicle, it must have at least Two properties and One method. You can decide on the method.
+// Create a subclass or "child" classes for a Car AND Truck.
+// Car must have at least One unique property.
+// Truck must have at least One unique property a& Must include the ability to Tow, with method to "activate 4 wheel drive".
+// Must have at least 2 instances of each Class.
+
 class Tesla {
     constructor(year, color) {
         this.year = year
--- codewars.js
+++ codewars.js
@@ -363,11 +363,7 @@ function makeNegative(num) {
 console.log(makeNegative(8))
 */
 
-// Create a class that represents a Vehicle, it must have at least Two properties and One method. You can decide on the method.
-// Create a subclass or "child" classes for a Car AND Truck.
-// Car must have at least One unique property.
-// Truck must have at least One unique property a& Must include the ability to Tow, with method to "activate 4 wheel drive".
-// Must have at least 2 instances of each Class.
+/* Not A CodeWars: Pretty Price
 
 function prettyPrice(x, y) {
     let newX = Math.floor(x)
@@ -376,3 +372,5 @@ function prettyPrice(x, y) {
 }
 
 console.log(prettyPrice(5.95, .99));
+
+*/
